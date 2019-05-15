4ddd0fe79d48df7dd417cfaef04143050f425a9c
aethros <aethros@users.noreply.github.com>
2019-05-14 20:39:26 -0400

    runic_close draft + changes
    -Incorporated comments
    --enum
    --safe_fstat
    -runic_file -> ro (match readme)
    -runic_close draft 1
    -runic_alloc_node outline/shell

--- runic.c
+++ runic.c
@@ -8,53 +8,81 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
-#include <fcntl.h>
 #include "runic.h"
 
 // TODO: Start by building API based on README.md
 // Go function by function. First three functions
 // listed.
-//
 
 // TODO: Research file offset and address. These mmap arguments are not correct or final!
+// File offset should be 0 if I am starting at the beginning of the file on every open. Not sure if it should be different but 0 for now.
+// Addr still not 100% on.
 runic_t runic_open(const char* path, int mode)
 {
-	runic_t runic_file;
+	runic_t ro;
 
 	switch (mode)
 	{
-	case 1: // Read-only
-		runic_file.fd = open(path, O_RDONLY);
-		if (fstat(runic_file.fd, &(runic_file.sb)) == -1)
-		{
-			perror("File access corrupted, couldn't get filesize.\n");
-			exit(1);
-		}
-		runic_file.addr = mmap(NULL, runic_file.sb.st_size, PROT_READ , MAP_PRIVATE, runic_file.fd, 0);
+	case readOnly: // Read-only
+		ro.fd = open(path, O_RDONLY);
+		safe_fstat(&ro);
+		ro.addr = mmap(NULL, ro.sb.st_size, PROT_READ , MAP_PRIVATE, ro.fd, 0);
+		
 		break;
-	
-	default: // Read-write, Create new
-		runic_file.fd = open(path, O_RDWR | O_CREAT | O_APPEND);
-		if (fstat(runic_file.fd, &(runic_file.sb)) == -1)
-		{
-			perror("File access corrupted, couldn't get filesize.\n");
-			exit(1);
-		}
-		runic_file.addr = mmap(NULL, runic_file.sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, runic_file.fd, 0);
+
+	case readWrite: // Read-write, Create new (default)
+	default:
+		ro.fd = open(path, O_RDWR | O_CREAT);
+		safe_fstat(&ro);
+		ro.addr = mmap(NULL, ro.sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, ro.fd, 0);
+		mmap_failed(ro);
 		break;
 	}	
 
-	return runic_file;
+	return ro;
+}
+void mmap_failed(runic_t ro)
+{
+	if(ro.addr == MAP_FAILED)
+	{
+		perror("Mmap failed.");
+		exit(1);
+	}
+}
+void safe_fstat(runic_t* ro)  // if there's a better naming convention i should use, just let me know
+{
+	if (fstat(ro->fd, &(ro->sb)) == -1)
+	{
+		perror("File access corrupted, couldn't get filesize.\n");
+		exit(1);
+	}
 }
 
 
-// - Write a function, given the aforementioned
-// - signature or similar, that closes a runic_file.
-// --- Start with a function that just closes a
-// --- runic_file using the mmap protocol.
-//
+void runic_close(runic_t ro)
+{
+	munmap(ro.addr, ro.sb.st_size);
+	close(ro.fd);
+}
+
+// alloc'ing a node is really a matter of alloc'ing file space for the node if none exists
+// remember these are mmap'd files
+// should it return an obj_t, or just alloc the space?
+runic_obj_t runic_alloc_node(runic_t ro)
+{
+	runic_obj_t rn;
+
+	rn.temp = 0; // change this later
+
+	return rn;
+}
+
+
+
 // - Write a function, given the aforementioned
 // - signature or similar, that inserts a root
 // - node into runic_file based on the passed handle.
--- runic.h
+++ runic.h
@@ -15,20 +15,35 @@ typedef struct runic
 {
 	int fd;
 	struct stat sb;
-	u_int8_t* addr;
+	uint8_t* addr;
 } runic_t;
+
+// incomplete. all c structs require one named member.
+typedef struct runic_obj
+{
+	int temp;
+} runic_obj_t;
+
+enum runic_file_modes // enums or #define?? it looks like #define is used for other flags
+{
+	readOnly, readWrite
+};
 
 // TODO: Start by building API based on README.md
 // Go function by function. First three functions
 // listed.
-//
+
 runic_t runic_open(const char* path, int mode);
-//
-// - Write a function, given the aforementioned
-// - signature or similar, that closes a file.
-// --- Start with a function that just closes a
-// --- file using the mmap protocol.
-//
+void mmap_failed(runic_t ro);
+void safe_fstat(runic_t* runic_file);
+
+void runic_close(runic_t runic_file);
+
+// alloc'ing a node is a matter of alloc'ing file space for the node if none exists
+// remember these are mmap'd files
+// should it return an obj_t, or just alloc the space?
+runic_obj_t runic_alloc_node(runic_t ro);
+
 // - Write a function, given the aforementioned
 // - signature or similar, that inserts a root
 // - node into file based on the passed handle.
